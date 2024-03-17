16f22621b33e1e5f420d86337aaa639325ad022f
andydluna <raidenshogun05@gmail.com>
2024-03-17 12:46:53 -0500

    updated fizzbuzz to have a while loop

--- 14 JavaScript/fizzBuzz.js
+++ 14 JavaScript/fizzBuzz.js
@@ -2,18 +2,21 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-    var item = count++;
-    
-    if (item % 3 == 0 && item % 5 == 0) {
-        item = "FizzBuzz";
+    while (count <= 100) {
+        var item = count++;
+        
+        if (item % 3 == 0 && item % 5 == 0) {
+            item = "FizzBuzz";
+        }
+        else if (item % 3 == 0) {
+            item = "Fizz";
+        }
+        else if (item % 5 == 0){
+            item = "Buzz";
+        }
+        
+        output.push(item);
     }
-    else if (item % 3 == 0) {
-        item = "Fizz";
-    }
-    else if (item % 5 == 0){
-        item = "Buzz";
-    }
-    
-    output.push(item);
+
     console.log(output);
 }
