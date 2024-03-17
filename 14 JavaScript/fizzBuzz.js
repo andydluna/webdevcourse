9300d07a22e017e4e2d70ae0984ce36d18ad7be1
andydluna <raidenshogun05@gmail.com>
2024-03-17 01:41:55 -0500

    added a fizzbuzz problem solution

---
+++ 14 JavaScript/fizzBuzz.js
@@ -0,0 +1,19 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    var item = count++;
+    
+    if (item % 3 == 0 && item % 5 == 0) {
+        item = "FizzBuzz";
+    }
+    else if (item % 3 == 0) {
+        item = "Fizz";
+    }
+    else if (item % 5 == 0){
+        item = "Buzz";
+    }
+    
+    output.push(item);
+    console.log(output);
+}
