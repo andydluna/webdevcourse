7f87eb1f38ebdffeb8e4397530485fc9d7e1502d
andydluna <raidenshogun05@gmail.com>
2024-03-21 19:31:07 -0500

    added the drum website :3

---
+++ 18 Advanced JavaScript/Drum Kit Starting Files/calculatorPractice.js
@@ -0,0 +1,8 @@
+function calculator(num1, num2, operator) {
+    return operator(num1, num2);
+}
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const divide = (num1, num2) => num1 / num2;
+const multiply = (num1, num2) => num1 * num2;
