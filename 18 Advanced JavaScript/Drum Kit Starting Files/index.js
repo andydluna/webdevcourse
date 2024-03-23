624fcba1aa0817839600695444cbb0e02378b095
andydluna <raidenshogun05@gmail.com>
2024-03-22 20:41:10 -0500

    added animation to the buttons

--- 18 Advanced JavaScript/Drum Kit Starting Files/index.js
+++ 18 Advanced JavaScript/Drum Kit Starting Files/index.js
@@ -35,14 +35,24 @@ function makeSound(key) {
     }
 }
 
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () { 
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
 for (var i = 0; i < buttonList.length; i++) {
     buttonList[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML)
     });
 }
 
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key)
 });
 
