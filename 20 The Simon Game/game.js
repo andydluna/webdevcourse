edcb58dc94e1362b24c4dcf83b139c1be8214748
andydluna <raidenshogun05@gmail.com>
2024-03-23 01:39:33 -0500

    added a simon game using JQuery!

---
+++ 20 The Simon Game/game.js
@@ -0,0 +1,68 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+        if (currentLevel == level - 1) {
+            setTimeout(function() {
+                userClickedPattern = [];
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function animatePress(currentColor) {
+    $("." + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("." + currentColor).removeClass("pressed")
+    }, 100);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function nextSequence() {
+    $("#level-title").text("Level " + ++level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+    $("." + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).on("keydown", function() {
+    if (level == 0) {
+        nextSequence();
+    }
+});
