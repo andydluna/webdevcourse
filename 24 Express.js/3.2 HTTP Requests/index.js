916f07476a95e38edb8daf9b5a2b6881c0f6a341
andydluna <raidenshogun05@gmail.com>
2024-04-08 00:49:54 -0500

    finished the express.js module

--- 24 Express.js/3.2 HTTP Requests/index.js
+++ 24 Express.js/3.2 HTTP Requests/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.send("<h1>Hello!</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me!</h1><p>Phone number: +1 (585) 553-0957</p>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me!</h1><p>My name is Andy, nice to meet you.</p>");
 });
 
 app.listen(port, () => {
